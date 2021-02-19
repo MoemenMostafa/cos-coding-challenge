f36f842fbb4b8711f71e74aac688b7c98312f484
Moamen Mostafa <moemen.a1@gmail.com>
2021-02-19 18:24:01 +0100

    adding real code to auth service

--- challenges/web-frontend/src/app/services/auth/auth.service.ts
+++ challenges/web-frontend/src/app/services/auth/auth.service.ts
@@ -1,9 +1,41 @@
 import { Injectable } from '@angular/core';
+import { BehaviorSubject, Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { HttpClient } from '@angular/common/http';
+import { Auth } from 'src/app/models/auth';
+import { environment } from 'src/environments/environment';
 
 @Injectable({
   providedIn: 'root'
 })
 export class AuthService {
 
-  constructor() { }
+    private loggedInUserSubject: BehaviorSubject<Auth>;
+    public loggedInUser: Observable<Auth>;
+
+    constructor(private http: HttpClient) {
+        this.loggedInUserSubject = new BehaviorSubject<Auth>(JSON.parse(localStorage.getItem('currentUser')));
+        this.loggedInUser = this.loggedInUserSubject.asObservable();
+    }
+
+    public get loggedInUserValue(): Auth {
+        return this.loggedInUserSubject.value;
+    }
+
+    login(userMailId: string, password: string) {
+        return this.http.put<any>(`${environment.apiUrl}/v1/authentication/${userMailId}`, { password })
+            .pipe(map(auth => {
+                if (auth && auth.token) {
+                    localStorage.setItem('currentUser', JSON.stringify(auth));
+                    this.loggedInUserSubject.next(auth);
+                }
+
+                return auth;
+            }));
+    }
+
+    logout() {
+        localStorage.removeItem('currentUser');
+        this.loggedInUserSubject.next(null);
+    }
 }
