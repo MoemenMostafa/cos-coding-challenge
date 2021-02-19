0798c8c70104137dac79046844ef86227a79eb1c
Moamen Mostafa <moemen.a1@gmail.com>
2021-02-19 11:17:24 +0100

    code clean up

--- challenges/web-frontend/src/app/pages/login/login.component.module.ts
+++ challenges/web-frontend/src/app/pages/login/login.component.module.ts
@@ -2,7 +2,6 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { LoginComponent } from './login.component';
 import { AppRoutingModule } from 'src/app/app-routing.module';
-import { RouterModule } from '@angular/router';
 import { LoginRoutingModule } from './login-routing.module';
 
 @NgModule({
