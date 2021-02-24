abca65621b376761997254682a94ce8be56d825d
Moamen Mostafa <moemen.a1@gmail.com>
2021-02-24 09:30:43 +0100

    remove the only console log

--- challenges/web-frontend/src/app/services/auctions/auctions.service.ts
+++ challenges/web-frontend/src/app/services/auctions/auctions.service.ts
@@ -12,7 +12,6 @@ export class AuctionsService {
   constructor(private http: HttpClient) { }
 
   public getAuctions() {
-    console.log('getAuctions');
     return timer(0, 20 * 1000).pipe(flatMap(() => {
       return this.http.get<any>(`${environment.apiUrl}/v2/auction/buyer/?count=false`)
         .pipe(map(auctions => {
